4cfba036c6b615d981e3bc3a4b185a01a1d1c099
Legitbunny <shreyashsingh18feb@gmail.com>
2024-04-10 01:47:41 +0530

    frontend url added in index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
     {
-        origin: ["https://socio-pedia-es7tdni2k-legitbunnys-projects.vercel.app/"],
+        origin: ["https://sociopedia-fe-by-legitbunny.netlify.app/"],
         methods: ["GET", "POST"],
     }
 ))
